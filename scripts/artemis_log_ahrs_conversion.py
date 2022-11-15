dc1188fd2b22c4a62562f4946432d1e5a007123b
FFT <fletho@dtu.dk>
2022-11-15 12:55:23 +0100

    Small refactoring of nodes and interface library

--- scripts/artemis_log_ahrs_conversion.py
+++ scripts/artemis_log_ahrs_conversion.py
@@ -2,14 +2,16 @@
 from artemis_ol.interface import LoggerReader
 import argparse
 from pathlib import Path
-from csv import reader, DictReader, writer
-from ahrs.filters import EKF, Complementary
+from csv import reader
+from ahrs.filters import EKF
 import numpy as np
 from scipy.spatial.transform import Rotation
+from scipy import signal
 import pandas as pd
 import warnings
 from matplotlib import pyplot as plt
 from scipy import signal as signal
+from ahrs.utils import WMM
 
 
 if __name__=="__main__":
@@ -18,8 +20,14 @@ if __name__=="__main__":
     ap.add_argument("-r", "--recursive", action="store_true", help="Search directories recursively for log files.")
     ap.add_argument("--plot", action="store_true", help="Flag to plot filtered data afterwards.")
     ap.add_argument("-f", "--frequency", type=float, default=100.0, help="Samping frequency.")
+    ap.add_argument("-d", "--disable_mag", action="store_true", help="Flag to omit magnetometer measurements.")
+    ap.add_argument("--sigma_a", type=float, default=0.07217913743449356, help="Standard deviation of accelerometer (m²/s⁴).")
+    ap.add_argument("--sigma_g", type=float, default=0.0294051717887628, help="Standard deviation of gyroscope (rad²/s²)")
+    ap.add_argument("--sigma_m", type=float, default=1.5530569285629352e-06, help="Standard deviation of magnetometer (T²).")
+    ap.add_argument("--latlon", nargs=2, type=float, default=[55.78452765101064, 12.51507263494401], help="Latitude Longitude for WMM")
     args = ap.parse_args()
     log_files = []
+    # Collect all the log files
     for log_file in args.log_file:
         if log_file.is_file():
             try:
@@ -35,12 +43,20 @@ if __name__=="__main__":
         else:
             warnings.warn(f"{log_file} neither a file nor a directory, skipping.")
             continue
+    # Filter out the log files with _ekf_ori_est as suffix in stem (avoids recursion)
     log_files = [log_file for log_file in log_files if "_ekf_ori_est" not in log_file.name]
+
+    # Lookup WMM dip angle
+    dip = WMM(latitude=args.latlon[0], longitude=args.latlon[1]).I
+
+    # Process each log file
     for log_file in log_files:
         print(f"Reprocessing orientation for {log_file}")
         acceleration = []
         angular_velocity = []
         magnetic_field = []
+        rpy = []
+        # Collect magnetometer data from LATIAS log
         with open(log_file, "r", newline="") as f:
             r = reader(f)
             for data in r:
@@ -48,11 +64,14 @@ if __name__=="__main__":
                 parsed_data = LoggerReader.parse_log_msg(logger_data)
                 if parsed_data is None:
                     continue
+                rpy.append([float(d) for d in data[-15:-12]])
                 acceleration.append([parsed_data[x] for x in ("ax", "ay", "az")])
                 angular_velocity.append([parsed_data[x] for x in ("wx", "wy", "wz")])
                 magnetic_field.append([parsed_data[x] for x in ("mx", "my", "mz")])
+        # if the log was empty, skip
         if not len(acceleration):
             continue
+        # Convert and calibrate acceleration
         acceleration = np.array(acceleration) / 1000.0 * 9.80865  # convert milli G to m/s/s
         acc_calib = np.array([0.997251, 0.000000, 0.000000, -0.008793,
                               0.000000, 0.996243, 0.000000, 0.111275,
@@ -62,15 +81,17 @@ if __name__=="__main__":
         acceleration = acc_calib @ acceleration.T
         acceleration = (acceleration[:3, :] / acceleration[3, :]).T
 
+        # Convert and calibrate angular velocity
         angular_velocity = np.array(angular_velocity) * np.pi / 180.0  # convert dps to rad/s
-        ang_calib = np.array([1.0, 0.000000, 0.000000, -0.014525851765573208,
-                              0.000000, 1.0, 0.000000, 6.702064327658279e-05,
-                              0.000000, 0.000000, 1.0, 0.004786565473594447,
+        ang_calib = np.array([1.0, 0.000000, 0.000000, 0.014525851765573208,
+                              0.000000, 1.0, 0.000000, -6.702064327658279e-05,
+                              0.000000, 0.000000, 1.0, -0.004786565473594447,
                               0.000000, 0.000000, 0.000000, 1.000000]).reshape((4, 4))
         angular_velocity = np.c_[angular_velocity, np.ones((angular_velocity.shape[0], 1))]
         angular_velocity = ang_calib @ angular_velocity.T
         angular_velocity = (angular_velocity[:3, :] / angular_velocity[3, :]).T
 
+        # Convert and calibrate magnetic field
         magnetic_field = np.array(magnetic_field) * 1e-6  # convert uT to T
         mag_calib = np.array([1.0728226719, 0.0090861975, 0.0092164637, -0.0000408731,
                               0.0090861975, 1.0336617766, -0.0181571431, -0.0000473644,
@@ -78,23 +99,31 @@ if __name__=="__main__":
                               0.0000000000, 0.0000000000, 0.0000000000, 1.0000000000]).reshape((4, 4))
         magnetic_field = np.c_[magnetic_field, np.ones((magnetic_field.shape[0], 1))]
         magnetic_field = mag_calib @ magnetic_field.T
-        magnetic_field = (magnetic_field[:3, :] / magnetic_field[3, :]).T * 1e9
+        magnetic_field = (magnetic_field[:3, :] / magnetic_field[3, :]).T * 1e6  # Convert to uT
 
-        # lowpass = 0.5  # Low pass frequency in Hz
-        # highpass = 0.01  # High pass frequency in Hz
-
-        # # Low pass filter on acceleration to remove impulse force spikes
-        # acceleration = signal.filtfilt(*signal.butter(5, lowpass, btype='low', fs=10), acceleration, axis=0)
-        # # High pass filter to remove sensor drift (low frequency)
-        # angular_velocity = signal.filtfilt(*signal.butter(5, highpass, btype='high', fs=10), angular_velocity, axis=0)
+        # Filter magnetic field spikes by applying a low pass filter to the magnetometer data
+        if magnetic_field.shape[0] > 18:
+            lowpass = 1  # Low pass frequency in Hz
+            magnetic_field = signal.filtfilt(*signal.butter(5, lowpass, btype='low', fs=args.frequency), magnetic_field, axis=0)
+        else:
+            warnings.warn("Could not low pass filter the magnetic field, effects of magnetometer spikes is much more pronounced.")
         
         print(f"Filtering orientation with EKF.")
-        ekf = EKF(angular_velocity, acceleration, magnetic_field,
-        frequency=args.frequency, var_gyr=0.0008646641279266516, var_acc=0.00520982788078751, var_mag=2.4119858233573378e-12)
+        if not args.disable_mag:
+            ekf = EKF(angular_velocity, acceleration, magnetic_field,
+            frequency=args.frequency, var_gyr=args.sigma_g, var_acc=args.sigma_a, var_mag=args.sigma_m * 1e12, magnetic_ref=dip, frame="ENU")
+        else:
+            ekf = EKF(angular_velocity, acceleration, frequency=args.frequency, var_gyr=args.sigma_g, var_acc=args.sigma_a, frame="ENU")
         print(f"Filtering done.")
         quat = ekf.Q
         R = Rotation.from_quat(ekf.Q[:, [1, 2, 3, 0]])
         euler = R.as_euler("xyz", degrees=True)
+        rpy = np.array(rpy)
+        # rpy is in N-WU, I will not defend this...
+        rpy[:, 1] = -rpy[:, 1]
+        # Rotate to ENU
+        R = Rotation.from_euler("xyz", (0,0,-90), degrees=True)
+        rpy = R.apply(rpy)
         if args.plot:
             fig, ax = plt.subplots(4, 1)
             ax[0].plot(acceleration)
@@ -102,12 +131,17 @@ if __name__=="__main__":
             ax[1].plot(angular_velocity)
             ax[1].set_ylabel("omega (rad/s)")
             ax[2].plot(magnetic_field)
-            ax[2].set_ylabel("mag (nT)")
-            ax[3].plot(euler)
+            ax[2].set_ylabel("mag (uT)")
+            colors = ["r", "g", "b"]
+            for i, e in enumerate(euler.T):
+                ax[3].plot(e, colors[i])
+            for i, e in enumerate(rpy.T):
+                ax[3].plot(e, colors[i]+"--")
             ax[3].set_ylabel("eul (deg)")
             plt.show()
         out_path = log_file.with_name(log_file.stem + "_ekf_ori_est" + log_file.suffix)
         data = pd.read_csv(log_file)
         data["Roll"], data["Pitch"], data["Yaw"] = euler.T
+        data["Qw"], data["Qx"], data["Qy"], data["Qz"] = quat.T
         print(f"Writing Roll, Pitch, Yaw Data to {out_path}.")
         data.to_csv(out_path, sep=",", index=False)
--- src/artemis_ol/interface.py
+++ src/artemis_ol/interface.py
@@ -5,11 +5,18 @@ import serial
 import threading
 import argparse
 from serial.tools import list_ports
-from typing import List, Union
+from typing import Union, Tuple
 from collections import deque
 import logging
 import datetime as dt
 import time
+import contextlib
+import re
+
+
+LOOPTIME_MS = 10e-6
+YEARMONTHDAY = re.compile("%d%d%d%d/%d%d/%d%d")
+DAYMONTHYEAR = re.compile("%d%d/%d%d/%d%d%d%d")
 
 
 def find_logger(vendor_id: Union[str, int], product_id: Union[str, int]) ->  Union[None, serial.tools.list_ports_linux.SysFS]:
@@ -35,6 +42,154 @@ def find_logger(vendor_id: Union[str, int], product_id: Union[str, int]) ->  Uni
         if vendor_flag and product_flag:
             return port
 
+
+@contextlib.contextmanager
+def nonblocking(lock):
+    locked = lock.acquire(False)
+    try:
+        yield locked
+    finally:
+        if locked:
+            lock.release()
+
+
+class LoggerManager(serial.Serial):
+    def __init__(self, vid: str="0x1a86", pid: str="0x7523", port: str=None, baudrate: int=115200, timeout: float=1.0, queue_size: int=None, log: bool=False):
+        if port is None:
+            device = None
+            if log:
+                    logging.info(f"Waiting for OL Artemis device connection {vid}:{pid}.")
+            while device is None:
+                device = find_logger(vid, pid)
+                time.sleep(LOOPTIME_MS)
+            logging.info(f"Connected to OL Artemis device.")
+            port = device.device
+        self._headers = None
+        self._serial_lock = threading.Lock()
+        self._log_flag = log
+        self._run_flag = False
+        self._in_buffer = deque(maxlen=queue_size)
+        self._out_buffer = deque(maxlen=queue_size)
+        self._read_thread = threading.Thread(target=self._read_serial)
+        self._write_thread = threading.Thread(target=self._write_serial)
+        serial.Serial.__init__(self, port, baudrate, timeout=timeout)
+    
+    def config(self):
+        self.send("h\n")
+        time.sleep(1e-1)
+        self.send("h\n")
+
+    def start(self):
+        if self._run_flag:
+            if self._log_flag:
+                logging.warn(f"Manager already started.")
+        else:
+            if self._log_flag:
+                logging.info(f"Starting manager.")
+            self._run_flag = True
+            self._read_thread.start()
+            self._write_thread.start()
+            self.config()
+
+    def stop(self):
+        if not self._run_flag:
+            if self._log_flag:
+                logging.warn(f"Manager already stopped.")
+        else:
+            if self._log_flag:
+                logging.info(f"Stopping manager.")
+            self._run_flag = False
+
+    def join(self):
+        self._read_thread.join()
+        self._write_thread.join()
+        self.close()
+
+    @staticmethod
+    def parse_headers(msg: str) -> Union[dict, tuple]:
+        if msg.lower().startswith("rtcdate"):
+            return dict(headers=tuple([m.lower() for m in msg.split(",") if len(m)]))
+        return tuple([m.lower() for m in msg.split(",") if len(m)])
+
+    @staticmethod
+    def parse_data(raw_data: Tuple[str], headers: Tuple[str, None]) -> Union[dict, None]:
+        imu_keys = ("ax", "ay", "az", "gx", "gy", "gz", "mx", "my", "mz", "rawax", "raway", "rawaz", "rawgx", "rawgy", "rawgz", "rawmx", "rawmy", "rawmz")
+        data = {header.lower(): packet for packet, header in zip(raw_data, headers)}
+        try:
+            date = data["rtcdate"]
+            rtctime = data["rtctime"]
+            ms_ = data.get("micros")
+            h, m, s = rtctime.split(":")
+            s, ms = s.split(".")
+            ms = int(ms)
+            # TODO: For now, micros and the rtctime centiseconds are not synced
+            if ms_ is not None:
+                ms = int(ms * 1e4 + int(ms_[-4:]))
+            datestring = "{}T{}{}{}.{:<06d}".format(date, h, m, s, ms)
+            if YEARMONTHDAY.match(date):
+                timestamp = dt.datetime.strptime(datestring, "%Y/%m/%dT%H%M%S.%f")
+            else:
+                timestamp = dt.datetime.strptime(datestring, "%d/%m/%YT%H%M%S.%f")
+            # timestamp = dt.datetime.strptime(datestring, "%Y/%m/%dT%H%M%S.%f")
+            imu_raw = {key: float(data.get(key)) for key in imu_keys if data.get(key) is not None}
+            imu_raw.update({"timestamp": timestamp})
+            return imu_raw
+        except KeyError as e:
+            raise KeyError(f"Logger Manager.parse_data {e}, {data}")
+        except ValueError as e:
+            raise ValueError(f"Logger Manager.parse_data {e}, {data}")
+        except AttributeError as e:
+            raise AttributeError(f"Logger Manager.parse_data {e}, {data}")
+
+    def _read_serial(self):
+        while self._run_flag:
+            if self._serial_lock.acquire(False):
+                try:
+                    line_bytes = self.readline()
+                    line = line_bytes.decode(encoding="utf-8").rstrip()
+                    parsed = LoggerManager.parse_headers(line)
+                    if isinstance(parsed, dict):
+                        if self._log_flag:
+                            logging.info(f"Update to headers: {parsed['headers']}")
+                        self._headers = parsed["headers"]
+                    elif self._headers is not None:
+                        data = LoggerManager.parse_data(parsed, self._headers)
+                        if data is not None:
+                            self._in_buffer.append(data)
+                    msg = None
+                except TimeoutError:
+                    msg = "Serial Timeout."
+                except UnicodeDecodeError:
+                    msg = "Decode error"
+                except ValueError:
+                    msg = "Value parse error."
+                except KeyError as e:
+                    msg = f"Key error. {e} {parsed}, {self._headers}"
+                finally:
+                    if self._log_flag:
+                        if msg is not None:
+                            logging.debug(msg)
+                self._serial_lock.release()
+            time.sleep(LOOPTIME_MS)
+    
+    def _write_serial(self):
+        while self._run_flag:
+            if self._serial_lock.acquire(False):
+                if len(self._out_buffer):
+                    packet = self._out_buffer.pop()
+                    if isinstance(packet, str):
+                        packet = packet.encode("utf-8")
+                    self.write(packet)
+                self._serial_lock.release()
+            time.sleep(LOOPTIME_MS)
+
+    def send(self, in_bytes: Union[bytes, str]):
+        self._out_buffer.append(in_bytes)
+
+    def __next__(self) -> Union[None, dict]:
+        if len(self._in_buffer):
+            return self._in_buffer.pop()
+            
 
 class LoggerReader(threading.Thread, serial.Serial):
     def __init__(self, vid: str="0x1a86", pid: str="0x7523", port: str=None, baudrate: int=115200, timeout: float=1.0, queue_size: int=None, log: bool=False):
@@ -63,7 +218,10 @@ class LoggerReader(threading.Thread, serial.Serial):
             datestring = "{}T{}{}{}.{:<06d}".format(date, h, m, s, int(ms))
         except ValueError as e:
             return
-        timestamp = dt.datetime.strptime(datestring, "%d/%m/%YT%H%M%S.%f")
+        if YEARMONTHDAY.match(date):
+            timestamp = dt.datetime.strptime(datestring, "%Y/%m/%dT%H%M%S.%f")
+        else:
+            timestamp = dt.datetime.strptime(datestring, "%d/%m/%YT%H%M%S.%f")
         return dict(ax=imu_data[0], ay=imu_data[1], az=imu_data[2],
         wx=imu_data[3], wy=imu_data[4], wz=imu_data[5],
         mx=imu_data[6], my=-imu_data[7], mz=-imu_data[8], ts=timestamp)
@@ -109,24 +267,26 @@ def main():
     filename = dt.datetime.utcnow().strftime("%y%m%dT%H%M%S.%f_artemis_ol.log")
     if args.logger_output == "log":
         logging.basicConfig(filename=filename, level=loglevel,
-        format='[%(asctime)s.%(msecs)06d %(levelname)s]\t%(message)s', datefmt="%y%m%dT%H%M%S")
+        format='[%(asctime)s.%(msecs)03d %(levelname)s]\t%(message)s', datefmt="%y%m%dT%H%M%S")
     else:
         logging.basicConfig(level=loglevel,
-        format='[%(asctime)s.%(msecs)06d %(levelname)s]\t%(message)s', datefmt="%y%m%dT%H%M%S")
+        format='[%(asctime)s.%(msecs)03d %(levelname)s]\t%(message)s', datefmt="%y%m%dT%H%M%S")
     log_flag = args.logger_output != "off"
-    time_window = deque(maxlen=100)
     try:
         sh = LoggerReader(args.vid, args.pid, args.port, args.baudrate, log=log_flag, queue_size=30)
+        sh = LoggerManager(args.vid, args.pid, args.port, args.baudrate, log=log_flag, queue_size=30)
         sh.start()
         log_last = time.perf_counter()
         counter = 0
         while True:
             if (time.perf_counter() - log_last) > 1:
-                print(counter)
+                if log_flag:
+                    logging.info(f"Average Rate: {counter} Hz")
                 counter = 0
                 log_last = time.perf_counter()
-            time.sleep(0.0000001)
-            if next(sh) is not None:
+            time.sleep(LOOPTIME_MS)
+            data = next(sh)
+            if data is not None:
                 counter = counter + 1
     except KeyboardInterrupt:
         if log_flag:
